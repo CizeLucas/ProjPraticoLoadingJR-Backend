c3762757ca7d13632dd8fcc314cacd0ad38d5d45
CizeLBook2 <cizelucas@gmail.com>
2024-05-21 10:46:12 -0300

    removed appsettins.json private config file

--- PublicationsAPI/Program.cs
+++ PublicationsAPI/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using PublicationsAPI.Data;
 using PublicationsAPI.Interfaces;
+using PublicationsAPI.Models;
 using PublicationsAPI.Repositories;
 using PublicationsAPI.Services.Users;
+using System.Text;
 
 namespace PublicationsAPI
 {
@@ -18,9 +22,36 @@ namespace PublicationsAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            
             builder.Services.AddDbContext<AppDBContext>(options => {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            
+            builder.Services.AddIdentity<Users, ApplicationRole>(options => {
+                options.Password.RequireDigit = true;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredUniqueChars = 0;
+            });
+
+            builder.Services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = "";
+                options.DefaultChallengeScheme = "";
+                options.DefaultScheme = "";
+                options.DefaultForbidScheme = "";
+                options.DefaultSignInScheme = "";
+                options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options => {
+                options.TokenValidationParameters = new TokenValidationParameters{
+                    ValidateIssuer = true,
+                    ValidIssuer = ,
+                    ValidateAudience = true,
+                    ValidAudience = ,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
+                    )
+                };
             });
 
             builder.Services.AddScoped<IUsersRepository, UsersRepository>();
@@ -37,6 +68,7 @@ namespace PublicationsAPI
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllers();
